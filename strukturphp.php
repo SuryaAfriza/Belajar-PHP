d6f54337a81464b22c2e05572b68a4f2c476a7c5
Surya Afriza <suryaafriza19@gmail.com>
2025-05-04 20:00:51 +0700

    membuat file strukturphp.php dan variable.php

---
+++ strukturphp.php
@@ -0,0 +1,6 @@
+<?php
+echo "Nama: Surya Afriza<br>";
+echo "NPM: 23081010268<br>";
+echo "Email: suryaafriza19@gmail.com<br>";
+echo "Alamat: Jl. Made AMD No.60<br>";
+?>
---
+++ variable.php
@@ -0,0 +1,22 @@
+<?php
+$nama="Ahmad Latif"; // String
+$nilai_bhs=2; // Integer
+$nilai_ipa=2.5; // Double
+$bentuk_boolean = true; // Boolean
+
+echo("Nama Mahasiswa= ".$nama."<br>");
+echo("Nilai Bahasa = ".$nilai_bhs."<br>");
+echo("Nilai IPA = ".$nilai_ipa."<br>");
+
+if($bentuk_boolean == true){
+    echo("Boolean Menunjukkan Nilai Benar <br>");
+}
+
+// Konversi ke float
+$nilai_bhs = (float)$nilai_bhs;
+// Konversi ke integer
+$nilai_ipa = (int)$nilai_ipa;
+
+echo("Nilai Bahasa setelah dikonversi ke float = ".$nilai_bhs."<br>");
+echo("Nilai IPA setelah dikonversi ke integer = ".$nilai_ipa."<br>");
+?>
