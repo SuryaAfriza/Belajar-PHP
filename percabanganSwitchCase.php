4bcc3a04e4cb1c5796ff13514cedc48bf847d04f
Surya Afriza <suryaafriza19@gmail.com>
2025-05-04 20:27:26 +0700

    membuat file percabanganSwitchCase.php

---
+++ percabanganSwitchCase.php
@@ -0,0 +1,14 @@
+<?php
+$hari_sekarang = date("l");
+
+switch($hari_sekarang) {
+    case "Monday": $hari = "Senin"; break;
+    case "Tuesday": $hari = "Selasa"; break;
+    case "Wednesday": $hari = "Rabu"; break;
+    case "Thursday": $hari = "Kamis"; break;
+    case "Friday": $hari = "Jumat"; break;
+    case "Saturday": $hari = "Sabtu"; break;
+    default: $hari = "Minggu";
+}
+echo "Sekarang Adalah Hari: $hari";
+?>
