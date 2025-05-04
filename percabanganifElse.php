5a332539c4dc19050161237c7bd00f3d204fcc25
Surya Afriza <suryaafriza19@gmail.com>
2025-05-04 20:24:22 +0700

    membuat file percabanganifElse.php

---
+++ percabanganifElse.php
@@ -0,0 +1,12 @@
+<?php
+$nilaipraktikum_php = 90;
+$nilaipraktikum_jarkom = 50;
+
+if ($nilaipraktikum_php > $nilaipraktikum_jarkom) {
+    echo 'Praktikum PHP lebih mudah';
+} elseif ($nilaipraktikum_php == $nilaipraktikum_jarkom) {
+    echo 'Praktikum PHP dengan Jarkom tingkat kesulitannya sama';
+} else {
+    echo 'Praktikum Jarkom lebih mudah';
+}
+?>
