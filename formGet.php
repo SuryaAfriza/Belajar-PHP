81e34d28550984431c541b31451525068d193e56
Surya Afriza <suryaafriza19@gmail.com>
2025-05-05 20:18:05 +0700

    membuat file formGet.php

---
+++ formGet.php
@@ -0,0 +1,6 @@
+<form name="form1" method="get" action="aksi_get.php">
+  Masukkan Nama Anda:
+  <input type="text" name="nama"> <br>
+  <input type="submit" name="Submit" value="Submit">
+  <input type="reset" name="Submit2" value="Reset">
+</form>
