9864f4825975e3f48b700ff383004385e5c574f2
Surya Afriza <suryaafriza19@gmail.com>
2025-05-05 19:49:30 +0700

    membuat file perulanganWhileDoWhile.php

---
+++ perulanganWhileDoWhile.php
@@ -0,0 +1,15 @@
+<?php
+// Contoh perulangan While
+$i = 1;
+while ($i <= 10) {
+    echo "Perulangan Ke-" . $i . "<br>";
+    $i++;
+}
+
+// Contoh perulangan Do-While
+$i = 1;
+do {
+    echo "Perulangan Ke-" . $i . "<br>";
+    $i++;
+} while ($i <= 10);
+?>
