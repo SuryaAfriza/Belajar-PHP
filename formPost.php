14a2c7adec8c1272f2c2849d26d87c34bb1dac06
Surya Afriza <suryaafriza19@gmail.com>
2025-05-05 20:20:54 +0700

    membuat file formPost.php

---
+++ formPost.php
@@ -0,0 +1,6 @@
+<form name="form1" method="post" action="aksi_post.php">
+  Masukkan Nama Anda:
+  <input type="text" name="nama"> <br>
+  <input type="submit" name="Submit" value="Submit">
+  <input type="reset" name="Submit2" value="Reset">
+</form>
